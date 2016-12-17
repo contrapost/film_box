48889109e82603e92acf14b15d3813190a3186cb
Alexander <shiale14@student.westerdals.no>
2016-12-17 12:12:32 +0100

    create app structure

---
+++ FilmBox/FilmBox/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FilmBox
+//
+//  Created by Alexander Shipunov on 17/12/2016.
+//  Copyright © 2016 Alexander Shipunov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
