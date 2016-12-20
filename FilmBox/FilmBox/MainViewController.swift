a57ea11bcbd84e5b2984847df019be1e13e6d37f
Alexander <shiale14@student.westerdals.no>
2016-12-20 16:24:25 +0100

    add film details

--- FilmBox/FilmBox/MainViewController.swift
+++ FilmBox/FilmBox/MainViewController.swift
@@ -105,14 +105,16 @@ class MainViewController: UIViewController, UITableViewDataSource, UITableViewDe
         return cell
     }
 
-    /*
     // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+        if segue.identifier == "segueToDetails" {
+            let indexPath = self.tableView.indexPathForSelectedRow?.row
+            
+            let viewController = segue.destination as! MovieDetailsViewController
+            
+            viewController.imdbID = filmsToShow[indexPath!].imdbID!
+        }
     }
-    */
 
 }
--- FilmBox/FilmBox/MovieDetailsViewController.swift
+++ FilmBox/FilmBox/MovieDetailsViewController.swift
@@ -7,14 +7,78 @@
 //
 
 import UIKit
+import CoreData
 
 class MovieDetailsViewController: UIViewController {
 
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var yearLabel: UILabel!
+    @IBOutlet weak var runtimeLabel: UILabel!
+    @IBOutlet weak var genreLable: UILabel!
+    @IBOutlet weak var imdbRatingLabel: UILabel!
+    @IBOutlet weak var lastSeenLable: UILabel!
+    @IBOutlet weak var navBar: UINavigationItem!
+    
+    var imdbID = String()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        loadData()
+        print(imdbID)
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    func loadData() {
+        let fetchRequest:NSFetchRequest<Film> = Film.fetchRequest()
+        fetchRequest.predicate = NSPredicate(format: "imdbID = %@", imdbID)
+        
+        do {
+            let fetchedFilms = try DatabaseController.getContext().fetch(fetchRequest)
+            let foundFilm = fetchedFilms[0] as Film
+            
+            titleLabel.text = foundFilm.title!
+            yearLabel.text = foundFilm.year!
+            runtimeLabel.text = foundFilm.runtime!
+            genreLable.text = foundFilm.genre!
+            imdbRatingLabel.text = foundFilm.imdbRating!
+            
+            if let actualLastSeen = foundFilm.lastSeen {
+                lastSeenLable.text = (actualLastSeen as Date).string(format: "dd-MM-yyyy")
+            } else {
+                lastSeenLable.text = "Not set"
+            }
+            
+            navBar.title = foundFilm.title!
+            
+        } catch {
+            print(error)
+        }
 
+    }
+
+    @IBAction func deleteFilm(_ sender: UIBarButtonItem) {
+        let fetchRequest:NSFetchRequest<Film> = Film.fetchRequest()
+        fetchRequest.predicate = NSPredicate(format: "imdbID = %@", imdbID)
+        
+        do {
+            let fetchedFilms = try DatabaseController.getContext().fetch(fetchRequest)
+            DatabaseController.getContext().delete(fetchedFilms[0])
+            do {
+                try DatabaseController.getContext().save()
+            } catch {
+                print(error)
+            }
+        } catch {
+            print(error)
+        }
+        
+        _ = navigationController?.popViewController(animated: true)
+        
+        print("Film with \(imdbID) has been deleted")
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -30,3 +94,11 @@ class MovieDetailsViewController: UIViewController {
     }
 }
 
+// http://stackoverflow.com/a/35170528/5552809
+extension Date {
+    func string(format: String) -> String {
+        let formatter = DateFormatter()
+        formatter.dateFormat = format
+        return formatter.string(from: self)
+    }
+}
